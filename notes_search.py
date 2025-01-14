5c479884a3adc6571efeb0a75e160565ad3cc869
Shuyuan Zhang <rayzhang707@gmail.com>
2025-01-14 20:16:34 +0000

    pdf search tuned

--- notes_search.py
+++ notes_search.py
@@ -4,25 +4,32 @@ import base64
 
 import whoosh_search as search
 
+st.set_page_config(
+    page_title="Notes Search and Visualization", 
+    layout="wide",
+    initial_sidebar_state="collapsed")
+
 
 # Title of the web app
 st.title("Notes Search and Visualization")
 
 # Input box for search query
 query = st.text_input("Enter search query:")
-
+_ = "🖼️"
 # Placeholder for search results
 if query:
     search_results = search.search_from_existing_index(query)
     st.write("Search results:")
     for result in search_results:
-        st.write(f"Tag: {result['tag']}")
-        st.write(f"Folder: {result['folder']}")
         pdf = search.retrieve_pdf_path(result['tag'], result['folder'])
-        if pdf:
+        pdf_path = pdf_path if pdf else "No PDF path found"
+        with st.expander(f"📄{result['tag']} | {result['folder']}"):
+            st.write(f"Tag: {result['tag']}")
+            st.write(f"Folder: {result['folder']}")
             st.write(f"PDF path: {pdf}")
+        if pdf:
             pdf_path_base64 = base64.b64encode(pdf.encode()).decode()
             if st.button("View PDF", key=pdf):
-                wb.open_new_tab(f"http://localhost:8501/pdf_display_tab?file={pdf_path_base64}")
+                wb.open_new_tab(f"http://localhost:8501/PDF?file={pdf_path_base64}")
         st.write("---")
     
--- pages/PDF.py
+++ pages/PDF.py
@@ -2,11 +2,12 @@ import streamlit as st
 import base64
 import webbrowser as wb
 
+st.set_page_config(layout="centered")
+
 def display_pdf(pdf_path):
     with open(pdf_path, "rb") as f:
         base64_pdf = base64.b64encode(f.read()).decode('utf-8')
-    pdf_display = f'<iframe src="data:application/pdf;base64,{base64_pdf}" width="700" height="1000" type="application/pdf"></iframe>'
-    # pdf_display = F'<embed src="data:application/pdf;base64,{base64_pdf}" width="700" height="1000" type="application/pdf">'
+    pdf_display = f'<div style="display: flex; justify-content: center;"><iframe src="data:application/pdf;base64,{base64_pdf}" width="90%" height="1000" type="application/pdf"></iframe></div>'
     st.markdown(pdf_display, unsafe_allow_html=True)
 
 page_params = st.query_params
@@ -15,5 +16,5 @@ if page_params:
     text = page_params.get("file", [""])
     if text:
         pdf_path = base64.b64decode(text).decode()
-        st.write(f"Displaying PDF at {pdf_path}")
+        st.sidebar.markdown(f"`{pdf_path}`")
         display_pdf(pdf_path)
